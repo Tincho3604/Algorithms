cbe9b11b1b3d17609d76b0172cb8405d33305c2a
Tincho3604 <tincho3604@gmail.com>
2021-08-30 01:00:10 -0300

    C# file

--- Programacion 3/Practicas en C#/Taller Mecanico/Taller Mecanico/CEjecutora.cs
+++ Programacion 3/Practicas en C#/Taller Mecanico/Taller Mecanico/CEjecutora.cs
@@ -14,27 +14,31 @@ namespace CAutoparte
             CAutoparte.setGanancia(float.Parse(Console.ReadLine()));
 
             CAutoparte parte = new CAutoparte(2015);
-            CAutoparte mayor = new CAutoparte(2015);
+            CAutoparte mayor = new CAutoparte(2016);
 
             Console.WriteLine("\n\n\t BIENVENIDO INGRESE LOS DATOS DE AUTPOARTES \t\n\n");
 
-                while (parte.CODIGO_DE_PIEZA != 0){
-                    
+                while (parte.CODIGO_DE_PIEZA != 0) {
+                
+                if (precioMasCaro < parte.darPrecio(12))
+                {
+                    Console.WriteLine("Precio mas caro anterior: " + precioMasCaro);
+                    precioMasCaro = parte.darPrecio(12);
+                    Console.WriteLine("Precio mas caro: " + precioMasCaro);
+                }
+                Console.WriteLine("\nIngrese Codigo de pieza: \n");
+                    parte.CODIGO_DE_PIEZA = Int32.Parse(Console.ReadLine());
+
                     Console.WriteLine("\nIngrese Descripccion de la autoparte: \n");
                     parte.DESCRIPCIÓN = Console.ReadLine();
 
                     Console.WriteLine("\nIngrese Costo de la autoparte: \n");
                     parte.COSTO = float.Parse(Console.ReadLine());
 
-                    Console.WriteLine("\nIngrese Codigo de pieza: \n");
-                    parte.CODIGO_DE_PIEZA = Int32.Parse(Console.ReadLine());
 
-
-                    if (precioMasCaro < parte.darPrecio(12)) {
-                        precioMasCaro = parte.darPrecio(12);
-                  }
-               } 
-            } 
+               }
+                Console.WriteLine("El costo total es: " + precioMasCaro);
+           }
         }
     }
 
