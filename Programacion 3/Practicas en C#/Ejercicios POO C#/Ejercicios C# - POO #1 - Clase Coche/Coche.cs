cf8fc2d6639d82f84b3f0e094fd3e67314325ff6
Tincho3604 <tincho3604@gmail.com>
2021-12-12 04:03:00 -0300

    add C# files

---
+++ Programacion 3/Practicas en C#/Ejercicios POO C#/Ejercicios C# - POO #1 - Clase Coche/Coche.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jueguito
+{
+    class Coche
+    {
+        private int _id;
+        private string _marca;
+        private string _modelo;
+        private int _KM;
+        private double _precio;
+
+        public Coche(int id, string marca, string modelo, int km, float precio)
+        {
+            _id = id;
+            _marca = marca;
+            _modelo = modelo;
+            _KM = km;
+            _precio = precio;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Marca { get => _marca; set => _marca = value; }
+        public string Modelo { get => _modelo; set => _modelo = value; }
+        public int KM { get => _KM; set => _KM = value; }
+        public double Precio { get => _precio; set => _precio = value; }
+
+        public override string ToString()
+        {
+            return "Marca: "+_marca+"Modelo: "+_modelo+"Precio: "+_precio;
+        }
+    }
+}
---
+++ Programacion 3/Practicas en C#/Ejercicios POO C#/Ejercicios C# - POO #1 - Clase Coche/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Jueguito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche e = new Coche(50, "Ford","2009",100, 12000);
+            Console.Write(e.Precio);
+
+            e.Precio = 2;
+
+            Console.WriteLine(e.ToString());
+
+        }
+    }
+}
